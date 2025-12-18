fb490f9939a85d5163747d1f3e4b0e447b8799b4
MuhammadShidqulIman <muhammadshidqul3@gmail.com>
2025-12-18 21:24:48 +0700

    Menambahkan MainActivity dengan setup Compose dan DataSiswaApp

--- app/src/main/java/com/example/mydatasiswa/MainActivity.kt
+++ app/src/main/java/com/example/mydatasiswa/MainActivity.kt
@@ -11,37 +11,21 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.mydatasiswa.ui.theme.MyDataSiswaTheme
+import com.example.mydatasisw.ui.theme.MydatasiswTheme
+import com.example.mydatasisw.uicontroller.DataSiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MyDataSiswaTheme {
+            MydatasiswaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DataSiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyDataSiswaTheme {
-        Greeting("Android")
     }
 }

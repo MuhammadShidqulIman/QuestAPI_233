47e21bab42645156b3a1782fe9cb9d93a30205c4
MuhammadShidqulIman <muhammadshidqul3@gmail.com>
2025-12-18 21:23:01 +0700

    Menambahkan PenyediaViewModel untuk HomeViewModel dan EntryViewModel

---
+++ app/src/main/java/com/example/mydatasiswa/viewmodel/provider/PenyediaViewModel.kt
@@ -0,0 +1,31 @@
+package com.example.mydatasisw.viewmodel.provider
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.mydatasisw.repositori.AplikasiDataSiswa
+import com.example.mydatasisw.viewmodel.EntryViewModel
+import com.example.mydatasisw.viewmodel.HomeViewModel
+
+fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as
+            AplikasiDataSiswa)
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(
+                aplikasiDataSiswa().container
+                    .repositoryDataSiswa
+            )
+        }
+        initializer {
+            EntryViewModel(
+                aplikasiDataSiswa().container
+                    .repositoryDataSiswa
+            )
+        }
+    }
+}

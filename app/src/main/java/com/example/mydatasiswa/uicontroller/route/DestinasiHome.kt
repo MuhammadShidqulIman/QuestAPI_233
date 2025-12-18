4a61bae6ac81f6a073d17351682a55da92623e10
MuhammadShidqulIman <muhammadshidqul3@gmail.com>
2025-12-18 21:58:04 +0700

    Menambahkan DestinasiHome

---
+++ app/src/main/java/com/example/mydatasiswa/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,7 @@
+package com.example.mydatasiswa.uicontroller.route
+
+import com.example.mydatasiswa.R
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
